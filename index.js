83b8cf9e2dc06158c8b7983807bad0e5c758407a
etherealfeel <graciagodsyndrome@gmail.com>
2024-01-27 22:15:40 +0200

    Init express server connect mongo db

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const connectionURL = process.env.CONNECTION_URL;
+const port = process.env.PORT || 4000;
+
+mongoose
+    .connect(connectionURL)
+    .then(() =>
+        app.listen(port, () => console.log(`Server running on port: ${port}`))
+    )
+    .catch((error) => console.log(error.message));
